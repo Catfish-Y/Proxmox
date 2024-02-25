2a895e231f20da2d619d83491496b1ae312cf849
Catfish-Y <35066569+Catfish-Y@users.noreply.github.com>
2024-02-25 22:31:49 +0800

    Update omv-install.sh

--- install/omv-install.sh
+++ install/omv-install.sh
@@ -30,11 +30,11 @@ msg_info "Installing OpenMediaVault (Patience)"
 wget -qO- https://packages.openmediavault.org/public/archive.key | gpg --dearmor >"/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg"
 
 cat <<EOF >/etc/apt/sources.list.d/openmediavault.list
-deb https://packages.openmediavault.org/public shaitan main
-# deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan main
+# deb https://packages.openmediavault.org/public shaitan main
+deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan main
 ## Uncomment the following line to add software from the proposed repository.
 # deb https://packages.openmediavault.org/public shaitan-proposed main
-# deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan-proposed main
+deb https://downloads.sourceforge.net/project/openmediavault/packages shaitan-proposed main
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
 # deb https://packages.openmediavault.org/public shaitan partner
