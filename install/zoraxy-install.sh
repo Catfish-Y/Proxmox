3b9ee7a5bf51743058d4eb0cee19754ea3e1d5af
Catfish-Y <35066569+Catfish-Y@users.noreply.github.com>
2024-10-31 14:12:30 +0800

    Update zoraxy-install.sh

--- install/zoraxy-install.sh
+++ install/zoraxy-install.sh
@@ -21,7 +21,7 @@ msg_ok "Installed Dependencies"
 
 msg_info "Installing Zoraxy (Patience)"
 RELEASE=$(curl -s https://api.github.com/repos/tobychui/zoraxy/releases/latest  | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-wget -q "https://github.com/tobychui/zoraxy/releases/download/${RELEASE}/zoraxy_linux_amd64"
+wget -q "https://ghp.ci/https://github.com/tobychui/zoraxy/releases/download/${RELEASE}/zoraxy_linux_amd64"
 mkdir -p /opt/zoraxy
 mv zoraxy_linux_amd64 /opt/zoraxy/zoraxy
 chmod +x /opt/zoraxy/zoraxy
